a1e5c02208c075ae6d416849f2c951d55cbc1450
Hank.Kuo <asdf024681029@gmail.com>
2023-06-05 23:10:25 +0800

    env

--- app.py
+++ app.py
@@ -29,5 +29,13 @@ def callback():
 
     return 'OK'
 
+@handler.add(MessageEvent, message=TextMessage)
+def echo(event):
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text)
+        )
+
 if __name__ == "__main__":
     app.run()
